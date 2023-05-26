cf8f36c10f906cb670b625ec3a5ec3ca6a3d7d87
Fabio Venturini <faventur@k1r3p11.42mulhouse.fr>
2023-05-26 12:33:36 +0200

    ready for a docker-compose

---
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sleep 10
+# https://linuxize.com/post/bash-check-if-file-exists
+FILE=./wp-config.php
+if [ ! -f "$FILE" ]; then
+	# https://developer.wordpress.org/cli/commands
+	wp config create	--allow-root \
+						--dbname=$SQL_DATABASE \
+						--dbuser=$SQL_USER \
+						--dbpass=$SQL_PASSWORD \
+						--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	wp core install		--url=$WP_DOMAIN_NAME \
+						--title=$WP_TITLE \
+						--admin_user=$WP_ADMIN_LOGIN \
+						--admin_password=$WP_ADMIN_PASSWORD \
+						--admin_email=$WP_ADMIN_MAIL
+	wp user create		$WP_USER_LOGIN \
+						$WP_USER_MAIL \
+						--user_pass=$WP_USER_PASSWORD
+						--role=$WP_USER_ROLE
+fi
