c39a8def3db4ce50c3206ae4eac57e0087a4585e
faventur <faventur.student.42mulhouse.fr>
2023-06-19 11:22:11 +0200

    manually mdb works

--- srcs/requirements/mariadb/tools/mysql_script.sh
+++ srcs/requirements/mariadb/tools/mysql_script.sh
@@ -5,8 +5,9 @@ mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" flush-privileges
+# mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
 exec mysqld_safe
 
 # https://dev.mysql.com/doc/refman/8.0/en/mysql-command-options.html#option_mysql_bind-address
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -8,19 +8,9 @@ if [ ! -e "$FILE" ]; then
 						--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 	sleep 10
-	wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-	wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
+	wp core install     --url=$WP_DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+	wp user create      --allow-root --role=author $WP_USER_LOGIN $WP_USER_MAIL --user_pass=$WP_USER_PASSWORD --path='/var/www/wordpress' >> /log.txt
 fi
-
-# echo "define( 'CONCATENATE_SCRIPTS', false );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'SCRIPT_DEBUG', true );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_HOME', 'https://mprigent.42.fr' );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_SITEURL', 'https://mprigent.42.fr' );" >> /var/www/wordpress/wp-config.php
-
-# echo "define( 'WP_DEBUG', true);" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_DEBUG_LOG', true);" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_DEBUG_DISPLAY', false);" >> /var/www/wordpress/wp-config.php
-# echo "define('WP_ALLOW_REPAIR', true);" >> /var/www/wordpress/wp-config.php
 
 # if /run/php folder doesn't exist, create it
 # https://stackoverflow.com/questions/43654656/dockerfile-if-else-condition-with-external-arguments
