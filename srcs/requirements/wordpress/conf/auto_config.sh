3d6a48c83304164f839b84448411c3b7d68fd1ab
Fabio Venturini <faventur@k2r3p4.42mulhouse.fr>
2023-06-19 08:34:33 +0200

    updates

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -3,26 +3,31 @@
 sleep 10
 # https://linuxize.com/post/bash-check-if-file-exists
 FILE=/var/www/wordpress/wp-config.php
-if [ ! -f "$FILE" ]; then
-	# https://developer.wordpress.org/cli/commands
-	wp core download --allow-root
+if [ ! -e "$FILE" ]; then
+	if [ ! -e /var/www/wordpress/wp-config.php ]; then
+    wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
+    					--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-	mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-	mv /wp-config.php /var/www/html/wp-config.php
-
-	sed -i -r "s/db1/$SQL_DATABASE/1" wp-config.php
-	sed -i -r "s/user/$SQL_USER/1" wp-config.php
-	sed -i -r "s/pwd/$SQL_PASSWORD/1" wp-config.php
-
-	wp core install		--allow-root \
-						--url=$WP_DOMAIN_NAME \
-						--title=$WP_TITLE \
-						--admin_user=$WP_ADMIN_LOGIN \
-						--admin_password=$WP_ADMIN_PASSWORD \
-						--admin_email=$WP_ADMIN_MAIL --path='/var/www/wordpress'
-	wp user create		$WP_USER_LOGIN \
-						$WP_USER_MAIL \
-						--user_pass=$WP_USER_PASSWORD \
-						--role=$WP_USER_ROLE
+sleep 10
+wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
 fi
+
+# echo "define( 'CONCATENATE_SCRIPTS', false );" >> /var/www/wordpress/wp-config.php
+# echo "define( 'SCRIPT_DEBUG', true );" >> /var/www/wordpress/wp-config.php
+# echo "define( 'WP_HOME', 'https://mprigent.42.fr' );" >> /var/www/wordpress/wp-config.php
+# echo "define( 'WP_SITEURL', 'https://mprigent.42.fr' );" >> /var/www/wordpress/wp-config.php
+
+# echo "define( 'WP_DEBUG', true);" >> /var/www/wordpress/wp-config.php
+# echo "define( 'WP_DEBUG_LOG', true);" >> /var/www/wordpress/wp-config.php
+# echo "define( 'WP_DEBUG_DISPLAY', false);" >> /var/www/wordpress/wp-config.php
+# echo "define('WP_ALLOW_REPAIR', true);" >> /var/www/wordpress/wp-config.php
+
+	
+
+# if /run/php folder doesn't exist, create it
+# https://stackoverflow.com/questions/43654656/dockerfile-if-else-condition-with-external-arguments
+if [ ! -d /run/php ]; then
+    mkdir ./run/php
+fi
+/usr/sbin/php-fpm7.3 -F
