6c06c347cbc908a270264cc5be6b429e8e3ecf88
faventur <faventur.student.42mulhouse.fr>
2023-07-28 11:35:14 +0200

    wp comments not recorded

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -24,16 +24,28 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 	sudo -u www-data wp user create      --role=author $USER1_LOGIN $USER1_MAIL \
 						--user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
 	
-	sudo -u www-data wp plugin install wp-redis --activate
-	sudo -u www-data wp redis enable
-    sudo -u www-data wp config set WP_REDIS_HOST "127.0.0.1"
-    sudo -u www-data wp config set WP_REDIS_PORT "6379"
-    sudo -u www-data wp config set WP_REDIS_DATABASE "15"
-    sudo -u www-data wp config set WP_REDIS_PASSWORD ${REDIS_PASSWORD}
+	# https://github.com/rhubarbgroup/redis-cache/blob/develop/INSTALL.md
+	# these commands install the Redis plugin for Wordpress
+	wp plugin install wp-redis --activate --allow-root
+	wp plugin update --all --allow-root
+
+	# adjust Redis host and port if necessary 
+	wp config set 'WP_REDIS_HOST', '127.0.0.1'
+	wp config set 'WP_REDIS_PORT', 6379
+
+	# change the prefix and database for each site to avoid cache data collisions
+	wp config set 'WP_REDIS_PREFIX', $SITE_TITLE
+	wp config set 'WP_REDIS_DATABASE', 0 # 0-15
+
+	# reasonable connection and read+write timeouts
+	wp config set 'WP_REDIS_TIMEOUT', 1
+	wp config set 'WP_REDIS_READ_TIMEOUT', 1
 fi
 
 # if /run/php folder does not exist, create it
 if [ ! -d /run/php ]; then
 	mkdir /run/php
 fi
+sudo -u www-data wp redis enable --allow-root
+
 /usr/sbin/php-fpm7.3 -F
